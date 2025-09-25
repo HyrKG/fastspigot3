7dfef9dae1e97e781a080e294078fb74945570fd
kg <end_kg@126.com>
2025-09-25 20:23:39 +0800

    feat(core): 添加 @Component 注解

---
+++ core/src/main/java/cn/hyrkg/fastspigot3/annotation/Component.java
@@ -0,0 +1,16 @@
+package cn.hyrkg.fastspigot3.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记为容器组件：被扫描到后将自动构造、注册并执行依赖注入与生命周期回调。
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Component {
+}
+
+
--- core/src/main/java/cn/hyrkg/fastspigot3/core/ScanService.java
+++ core/src/main/java/cn/hyrkg/fastspigot3/core/ScanService.java
@@ -3,6 +3,7 @@ package cn.hyrkg.fastspigot3.core;
 import cn.hyrkg.fastspigot3.annotation.Inject;
 import cn.hyrkg.fastspigot3.annotation.Wire;
 import cn.hyrkg.fastspigot3.scanner.ClassScanner;
+import cn.hyrkg.fastspigot3.annotation.Component;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -26,6 +27,9 @@ public class ScanService {
             clazz.getDeclaredConstructor();
         } catch (NoSuchMethodException e) {
             return false;
+        }
+        if (clazz.isAnnotationPresent(Component.class)) {
+            return true;
         }
         for (Field field : clazz.getDeclaredFields()) {
             if (field.isAnnotationPresent(Inject.class) || field.isAnnotationPresent(Wire.class)) {
