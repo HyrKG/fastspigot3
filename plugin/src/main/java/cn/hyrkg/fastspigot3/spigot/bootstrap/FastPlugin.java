52d0cb0b9772747730c4c9025c954ec4df8a1b96
kg <end_kg@126.com>
2025-09-27 04:45:07 +0800

    feat(plugin):[bootstrap]初始化ApplicationContext

--- plugin/src/main/java/cn/hyrkg/fastspigot3/spigot/bootstrap/FastPlugin.java
+++ plugin/src/main/java/cn/hyrkg/fastspigot3/spigot/bootstrap/FastPlugin.java
@@ -1,20 +1,31 @@
 package cn.hyrkg.fastspigot3.spigot.bootstrap;
 
+import cn.hyrkg.fastspigot3.context.ApplicationContext;
 import cn.hyrkg.fastspigot3.spigot.AppFastSpigot;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FastPlugin extends JavaPlugin {
 
+    @Getter
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void onLoad() {
+        applicationContext = new ApplicationContext();
+    }
 
     @Override
     public void onEnable() {
-        AppFastSpigot.getApplicationContext().registerBeanInstance(getClass(), this);
-        AppFastSpigot.getApplicationContext().scanAndRegister(getClass().getPackage().getName(), getClass());
+        applicationContext.scanAndRegister(AppFastSpigot.class.getPackage().getName());
+        applicationContext.registerBeanInstance(getClass(), this);
+        applicationContext.scanAndRegister(getClass().getPackage().getName(), getClass());
     }
 
     @Override
     public void onDisable() {
-        AppFastSpigot.getApplicationContext().scanAndUnregister(getClass().getPackage().getName(), getClass());
-        AppFastSpigot.getApplicationContext().unregisterBeanInstance(getClass());
+        applicationContext.scanAndUnregister(getClass().getPackage().getName(), getClass());
+        applicationContext.unregisterBeanInstance(getClass());
+        applicationContext.scanAndUnregister(AppFastSpigot.class.getPackage().getName());
     }
 }
