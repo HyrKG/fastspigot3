b0473013a84ceedd5b0774e5ba54f5d1d0a3b693
kg <end_kg@126.com>
2025-09-28 00:19:46 +0800

    feat(plugin): 引入插件专属日志系统

---
+++ plugin/src/main/java/cn/hyrkg/fastspigot3/spigot/logger/DefaultLogger.java
@@ -0,0 +1,74 @@
+package cn.hyrkg.fastspigot3.spigot.logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
+
+public class DefaultLogger implements Logger {
+
+    private final JavaPlugin plugin;
+    private final Field field;
+    private String pluginName;
+    private String path;
+
+    public DefaultLogger(JavaPlugin plugin, Field field) {
+
+        this.plugin = plugin;
+        this.field = field;
+
+        setup();
+    }
+
+    private void setup() {
+        Class<?> declaringClass = field.getDeclaringClass();
+        String fullClassName = declaringClass.getName();
+        String[] parts = fullClassName.split("\\.");
+
+        StringBuilder pathBuilder = new StringBuilder();
+
+        // 处理包名部分，每个包名只保留第一个字母
+        for (int i = 0; i < parts.length - 1; i++) {
+            pathBuilder.append(parts[i].charAt(0)).append(".");
+        }
+
+        // 添加类名
+        pathBuilder.append(parts[parts.length - 1]);
+
+        // 设置路径
+        this.path = pathBuilder.toString();
+        this.pluginName = plugin.getName();
+    }
+
+    @Override
+    public void debug(String message) {
+        Bukkit.getConsoleSender().sendMessage(format("D", message, "§f"));
+    }
+
+    @Override
+    public void info(String message) {
+        Bukkit.getConsoleSender().sendMessage(format("I", message, "§a"));
+    }
+
+    @Override
+    public void notice(String message) {
+        Bukkit.getConsoleSender().sendMessage(format("N", message, "§9"));
+    }
+
+    @Override
+    public void warn(String message) {
+        Bukkit.getConsoleSender().sendMessage(format("W", message, "§e"));
+    }
+
+    @Override
+    public void error(String message) {
+        Bukkit.getConsoleSender().sendMessage(format("E", message, "§4"));
+    }
+
+
+    private String format(String prefix, String message, String color) {
+        String format = "§7[%s] %s(%s)%s: %s§r";
+        return String.format(format, pluginName, color, prefix, path, message);
+    }
+
+}
---
+++ plugin/src/main/java/cn/hyrkg/fastspigot3/spigot/logger/LoggerProcessor.java
@@ -0,0 +1,36 @@
+package cn.hyrkg.fastspigot3.spigot.logger;
+
+import cn.hyrkg.fastspigot3.beans.factory.BeanFactory;
+import cn.hyrkg.fastspigot3.beans.factory.support.BeanDefinitionRegistry;
+import cn.hyrkg.fastspigot3.context.annotation.Autowired;
+import cn.hyrkg.fastspigot3.context.annotation.processor.FieldAnnotationProcessor;
+import cn.hyrkg.fastspigot3.context.annotation.processor.ProcessFieldForAnnotation;
+import cn.hyrkg.fastspigot3.context.annotation.processor.ProcessorAction;
+import cn.hyrkg.fastspigot3.spigot.util.PluginUtils;
+import cn.hyrkg.fastspigot3.stereotype.Component;
+import cn.hyrkg.fastspigot3.util.ReflectionUtils;
+import lombok.SneakyThrows;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
+
+@Component
+@ProcessFieldForAnnotation(Autowired.class)
+public class LoggerProcessor implements FieldAnnotationProcessor<Autowired> {
+
+    @Override
+    @SneakyThrows
+    public ProcessorAction preProcess(Field field, Autowired annotation, Object bean) {
+        if (field.getType().equals(Logger.class)) {
+            Logger logger = getLogger(field, bean);
+            ReflectionUtils.setFieldValue(field, bean, logger);
+            return ProcessorAction.STOP_PROCESSING;
+        }
+        return ProcessorAction.CONTINUE;
+    }
+
+    public Logger getLogger(Field field, Object bean) {
+        JavaPlugin pluginFromObject = PluginUtils.getPluginFromObject(bean);
+        return new DefaultLogger(pluginFromObject, field);
+    }
+}
