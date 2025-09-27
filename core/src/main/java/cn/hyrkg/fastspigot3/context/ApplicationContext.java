7e61709ae4895cc194bf2d20547e11123bef63a5
kg <end_kg@126.com>
2025-09-28 01:36:47 +0800

    feat(core): [context]app启动支持回传log信息

--- core/src/main/java/cn/hyrkg/fastspigot3/context/ApplicationContext.java
+++ core/src/main/java/cn/hyrkg/fastspigot3/context/ApplicationContext.java
@@ -5,9 +5,11 @@ import cn.hyrkg.fastspigot3.beans.factory.config.BeanDefinition;
 import cn.hyrkg.fastspigot3.context.annotation.AnnotationComponentClassScanner;
 import cn.hyrkg.fastspigot3.context.support.BeanDependencyResolver;
 import cn.hyrkg.fastspigot3.scanner.Scanner;
+import lombok.Getter;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * 应用上下文（门面）：对外提供统一的扫描、装配与 Bean 访问能力。
@@ -15,9 +17,16 @@ import java.util.List;
  */
 public class ApplicationContext {
 
+    @Getter
     private final DefaultBeanDefinitionRegistry beanFactory = new DefaultBeanDefinitionRegistry();
     private final Scanner beanScanner = new AnnotationComponentClassScanner();
     private final BeanDependencyResolver dependencyResolver = new BeanDependencyResolver();
+
+    private Consumer<String> logConsumer = null;
+
+    public void setLogConsumer(Consumer<String> logConsumer) {
+        this.logConsumer = logConsumer;
+    }
 
     public void scanAndRegister(String basePackage) {
         scanAndRegister(basePackage, null);
@@ -32,8 +41,10 @@ public class ApplicationContext {
         }
         componentClass = dependencyResolver.resolveOrder(componentClass);
         for (Class<?> clazz : componentClass) {
+            long start = System.currentTimeMillis();
             BeanDefinition beanDefinition = beanFactory.registerBean(clazz);
             beanFactory.loadBean(beanDefinition.getBeanName());
+            log("..." + beanDefinition.getBeanName() + " (" + (System.currentTimeMillis() - start) + " ms)");
         }
     }
 
@@ -58,9 +69,12 @@ public class ApplicationContext {
         }
     }
 
-    public DefaultBeanDefinitionRegistry getBeanFactory() {
-        return beanFactory;
+    private void log(String message) {
+        if (logConsumer != null) {
+            logConsumer.accept(message);
+        }
     }
+
 }
 
 
